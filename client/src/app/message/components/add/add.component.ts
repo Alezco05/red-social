b03710cdf0da57bfa42eb3abf254649bdf1a434b
Alezco05 <jhon.mario.05@hotmail.com>
2019-04-26 15:31:46 -0500

    Enviando mensajes

--- client/src/app/message/components/add/add.component.ts
+++ client/src/app/message/components/add/add.component.ts
@@ -38,5 +38,30 @@ export class AddComponent implements OnInit{
     }
     ngOnInit(){
         console.log('main.component cargado');
+        this.getMyFollows();
     }
+    onSubmit(form){
+		this._messageService.addMessage(this.token, this.message).subscribe(
+			response => {
+				if(response.message){
+					this.status = 'success';
+					form.reset();
+				}
+			},
+			error => {
+				this.status = 'error';
+				console.log(<any>error);
+			}
+		);
+	}
+    getMyFollows(){
+		this._followService.getMyFollows(this.token).subscribe(
+			response => {	
+				this.follows = response.follows;
+			},
+			error => {
+				console.log(<any>error);
+			}
+		);
+	}
 }
--- client/src/app/services/message.service.ts
+++ client/src/app/services/message.service.ts
@@ -6,23 +6,28 @@ import {Message} from '../models/message';
 @Injectable()
 export class MessageService{
     public url: string;
-    constructor(private _htpp: HttpClient){
+    constructor(private _http: HttpClient){
         this.url = GLOBAL.url;
     }
-    addMessage(token,message):Observable<any>{
-        let params = JSON.stringify(message);
-        let headers = new HttpHeaders().set('Content-Type','application/json')
-        .set('Authorization',token);
-        return this._htpp.post(this.url+'message',params,{headers:headers});
-    }
-    getMyMessage(token,page=1):Observable<any>{
-        let headers = new HttpHeaders().set('Content-Type','application/json')
-        .set('Authorization',token);
-        return this._htpp.get(this.url+'my-messages/'+page,{headers:headers});
-    }
-    getEmitMessages(token,page=1):Observable<any>{
-        let headers = new HttpHeaders().set('Content-Type','application/json')
-        .set('Authorization',token);
-        return this._htpp.get(this.url+'messages/'+page,{headers:headers});
-    }
+   addMessage(token, message):Observable<any>{
+		let params = JSON.stringify(message);
+		let headers = new HttpHeaders().set('Content-Type','application/json')
+									   .set('Authorization',token);
+
+		return this._http.post(this.url+'message', params, {headers: headers});
+	}
+
+	getMyMessages(token, page = 1):Observable<any>{
+		let headers = new HttpHeaders().set('Content-Type','application/json')
+									   .set('Authorization',token);
+
+	   return this._http.get(this.url+'my-messages/'+page, {headers: headers});
+	}
+
+	getEmmitMessages(token, page = 1):Observable<any>{
+		let headers = new HttpHeaders().set('Content-Type','application/json')
+									   .set('Authorization',token);
+
+	   return this._http.get(this.url+'messages/'+page, {headers: headers});
+	}
 }
